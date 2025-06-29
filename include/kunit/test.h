8bae6dec98b851bd1e0c5db8fce587a71b7a2792
AKM <akmabrar04@gmail.com>
2025-06-29 17:39:25 +0600

    include: Import Samsung OEM Changes from (gki-common) OSRC S9180ZHU6DYD9
    Signed-off-by: AKM <akmabrar04@gmail.com>

--- include/kunit/test.h
+++ include/kunit/test.h
@@ -10,7 +10,9 @@
 #define _KUNIT_TEST_H
 
 #include <kunit/assert.h>
+#ifdef CONFIG_SEC_KUNIT
 #include <kunit/kunit-stream.h>
+#endif /* CONFIG_SEC_KUNIT */
 #include <kunit/try-catch.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -277,7 +279,9 @@ struct kunit {
 	 * protect it with some type of lock.
 	 */
 	struct list_head resources; /* Protected by lock. */
+#ifdef CONFIG_SEC_KUNIT
 	struct list_head post_conditions;
+#endif /* CONFIG_SEC_KUNIT */
 	char status_comment[KUNIT_STATUS_COMMENT_SIZE];
 };
 
@@ -1882,6 +1886,7 @@ do {									       \
 		}										\
 		return NULL;									\
 	}
+#ifdef CONFIG_SEC_KUNIT
 /*
  * for mock feature from kunit/alpha/master
  */
@@ -1908,5 +1913,6 @@ void test_install_initcall(struct test_initcall *initcall);
 			return 0; \
 		} \
 		test_pure_initcall(register_test_initcall_##initcall)
+#endif /* CONFIG_SEC_KUNIT */
 
 #endif /* _KUNIT_TEST_H */
